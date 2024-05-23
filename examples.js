5a6392629231ca1f99d9dc29df09335627c7bba9
Vitor Oliveira. <caulicons@gmail.com>
2024-05-23 18:10:09 -0300

    Um comite muito dahora
    Co-authored-by: Marcelo Henrique <Marshel0@users.noreply.github.com>

--- examples.js
+++ examples.js
@@ -1,35 +1,3 @@
-const leia = require("readline-sync")
-
-const fila = ["Bainca", "Bianca1", "Bianca2"]
-
-while (true) {
-
-  console.log(`
-  1 - Adicionar Na fila
-  2 - Lista a fila 
-  3  - Remover da fila
-  0 - Sair
-  `)
-
-
-  console.log("Escolhar uma opção.")
-
-  const opcoe = leia.questionInt();
-
-  if (opcoe == 0) {
-    process.exit(0)
-  }
-
-  if (opcoe == 1) {
-
-    console.log("Qual o nome da pessoa ?")
-    const nome = leia.question();
-
-    fila.push(nome);
-
-    fila.forEach(pessoa => console.log(pessoa))
-
-
-  }
-
-}
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}dsadds asdfsadfsd
